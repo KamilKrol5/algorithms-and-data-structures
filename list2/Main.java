5479515ae24964a6af45b3194c25ba2c1093a57e
Kamil Król <kamil55525@gmail.com>
2019-03-27 02:13:56 +0100

    Implement quick sort, heap sort and modified quick sort

--- list2/Main.java
+++ list2/Main.java
@@ -7,10 +7,13 @@ public class Main {
         System.out.println("Hello World!");
         var list1 = new ArrayList<>(Arrays.asList(1,9,2,3,5,7,0));
         var list2 = new ArrayList<>(Arrays.asList(10,9,2,30,5,70,0,41,42,12,10,10));
+        var list3 = new ArrayList<>(Arrays.asList(11,9,0,30,5,-8,0,41,42,12,10,4,5,1,2));
         var so = new SortingAlgorithms(false,false);
-        so.insertionSort(list1);
-        so.insertionSort(list2);
+        so.modifiedQuickSort(list1);
+        so.modifiedQuickSort(list2);
+        so.modifiedQuickSort(list3);
         System.out.println(list1);
         System.out.println(list2);
+        System.out.println(list3);
     }
 }
--- list2/SortingAlgorithms.java
+++ list2/SortingAlgorithms.java
@@ -38,23 +38,103 @@ class SortingAlgorithms {
     }
 
     void heapSort(List<Integer> data) {
-
+        makeHeap(data);
+        int size = data.size();
+        for (int j = 0; j < data.size(); j++) {
+            swap(data,size-j-1,0);
+            siftDown(data.subList(0,size-j-1), 0);
+        }
     }
+
 
     void quickSort(List<Integer> data) {
-
+        if (data.size() >=  1){
+            int pivot = data.get(data.size() - 1);
+            int limit = 0;
+            for (int i = 0; i < data.size()-1; i++) {
+                if ( comparator.compare(pivot,data.get(i)) >= 0) {
+                    swap(data,limit,i);
+                    limit++;
+                }
+            }
+            swap(data,data.size() - 1,limit);
+            quickSort(data.subList(0,limit));
+            quickSort(data.subList(limit+1,data.size()));
+        }
     }
 
-    private Boolean compare(Integer first, Integer second) {
-        if (!isSilentMode) System.err.println("Compare " + first + " with " + second);
-        if (descending) return first <= second;
-        else return first >= second;
+    public void modifiedQuickSort(List<Integer> list) {
+        int size = list.size();
+        if (size >=  1){
+            if (size > 16) {
+                int pivotIndex = getMedianIndex(list, 0, size / 2 - 1, size - 1);
+                int pivot = list.get(pivotIndex);
+                swap(list, pivotIndex, size - 1);
+                int limit = 0;
+                for (int i = 0; i < size - 1; i++) {
+                    if (comparator.compare(pivot, list.get(i)) >= 0) {
+                        swap(list, limit, i);
+                        limit++;
+                    }
+                }
+                swap(list, size - 1, limit);
+                quickSort(list.subList(0, limit));
+                quickSort(list.subList(limit + 1, size));
+            } else {
+                insertionSort(list);
+            }
+        }
     }
 
     private void swap(List<Integer> list, int index1, int index2) {
         Integer tmp = list.get(index1);
         list.set(index1,list.get(index2));
         list.set(index2,tmp);
+    }
+
+    private void makeHeap(List<Integer> data) {
+        int i = data.size() / 2 - 1;
+        for (int k = i; k >= 0; k--) {
+            siftDown(data,k);
+        }
+    }
+
+    private void siftDown(List<Integer> list, int index) {
+        int currentIndex, leftChild, rightChild;
+        int size = list.size();
+        while (index < size) {
+            currentIndex = index;
+
+            leftChild = 2 * currentIndex + 1;
+            rightChild = leftChild + 1;
+
+            if (leftChild < size && comparator.compare(list.get(leftChild),list.get(currentIndex)) > 0)
+                currentIndex = leftChild;
+            if (rightChild < size && comparator.compare(list.get(rightChild),list.get(currentIndex)) > 0)
+                currentIndex = rightChild;
+
+            if (currentIndex == index)
+                return;
+
+            swap(list,currentIndex,index);
+
+            index = currentIndex;
+        }
+    }
+
+    public int getMedianIndex(List<Integer> list, Integer i1,Integer i2,Integer i3) {
+        int x = list.get(i1);
+        int y = list.get(i2);
+        int z = list.get(i3);
+        if (comparator.compare(x,y) >=0) {
+            if (comparator.compare(y, z) >= 0) return i2;
+            else if (comparator.compare(x,z) >= 0) return i3;
+            else return i1;
+        } else {
+            if (comparator.compare(x,z) >= 0) return i1;
+            else if (comparator.compare(y,z) >= 0) return i3;
+            else return i2;
+        }
     }
 
     private Pair<Integer, Integer> findMin(List<Integer> list) {
