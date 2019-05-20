523596841ff64ce9f748874c812f70584797e936
Kamil <kamil55525@gmail.com>
2019-05-20 22:23:40 +0200

    Fix search operation and add stat operation to TreeInputHandler

--- list4/src/utility/TreeInputHandler.java
+++ list4/src/utility/TreeInputHandler.java
@@ -11,7 +11,8 @@ public class TreeInputHandler {
     private final static String message = "Choose one of the operations:\n" +
             "i x (insert x); io (in order)\n" +
             "d x (delete x); s x (search x)\n" +
-            "e (is empty);   p (print)\n";
+            "e (is empty);   p (print)\n" +
+            "stat (show statistics)\n";
 
     public static <T extends Comparable<T>> void handleInputInt(AbstractTree<T> tree,
                                                                 InputStream inputStream,
@@ -39,7 +40,7 @@ public class TreeInputHandler {
                     break;
                 case "s":
                     if (scanner.hasNext())
-                        tree.search(fromString.apply(scanner.next()));
+                        printStream.println(tree.search(fromString.apply(scanner.next())));
                     else
                         printStream.println("Wrong input provided.");
                     break;
@@ -51,6 +52,16 @@ public class TreeInputHandler {
                     break;
                 case "io":
                     printStream.println(tree.inOrder());
+                    break;
+                case "stat":
+                    printStream.println("Current number of elements:" + tree.getCurrentNumberOfElements());
+                    printStream.println("Maximum number of :" + tree.getMaximumNumberOfElements());
+                    printStream.println("Number of comparisons:" + tree.getNumberOfComparisons());
+                    printStream.println("Number of inserts:" + tree.getNumberOfInserts());
+                    printStream.println("Number of deletions:" + tree.getNumberOfDeletions());
+                    printStream.println("Number of inOrder calls:" + tree.getNumberOfInOrders());
+                    printStream.println("Number of searches:" + tree.getNumberOfSearches());
+                    printStream.println("Number of loads:" + tree.getNumberOfLoads());
                     break;
                 case "exit":
                     System.exit(0);
