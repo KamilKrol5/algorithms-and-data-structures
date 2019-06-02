b01db9b7d1c35eeb2b3de91299d29f04af7054ae
Kamil <kamil55525@gmail.com>
2019-06-02 19:46:59 +0200

    Add showing average statistics for operation to AbstractTreeExtensions

--- list4/src/utility/AbstractTreeExtensions.java
+++ list4/src/utility/AbstractTreeExtensions.java
@@ -59,6 +59,7 @@ public class AbstractTreeExtensions {
         System.out.println("Current number of elements:" + t.getCurrentNumberOfElements());
         System.out.println("Maximum number of elements:" + t.getMaximumNumberOfElements());
         System.out.println("Number of comparisons:" + t.getNumberOfComparisons());
+        System.out.println("Number of modifications:" + t.getNumberOfNodeModifications());
         System.out.println("Number of inserts:" + t.getNumberOfInserts());
         System.out.println("Number of deletions:" + t.getNumberOfDeletions());
         System.out.println("Number of inOrder calls:" + t.getNumberOfInOrders());
@@ -66,10 +67,34 @@ public class AbstractTreeExtensions {
         System.out.println("Number of loads:" + t.getNumberOfLoads());
     }
 
+    public static <T extends Comparable<T>> void showChosenStatistics(AbstractTree<T> t) {
+//        System.out.println("Current number of elements:" + t.getCurrentNumberOfElements());
+//        System.out.println("Number of comparisons:" + t.getNumberOfComparisons());
+//        System.out.println("Number of modifications:" + t.getNumberOfNodeModifications());
+        System.out.println(t.getNumberOfComparisons());
+        System.out.println(t.getNumberOfNodeModifications());
+
+    }
+
+    public static <T extends Comparable<T>> void showAverageStatisticsForOperation(AbstractTree<T> t, String operationName, long numberOfOperationCalls, long execTime) {
+        showAverageStatisticsForOperation(t, operationName, numberOfOperationCalls);
+//        System.out.println("Average time per one " + operationName + ": " + (double)execTime / numberOfOperationCalls + " milliseconds");
+        System.out.printf("%.3fms\n", (double)execTime / numberOfOperationCalls);
+        System.out.println();
+    }
+
+    public static <T extends Comparable<T>> void showAverageStatisticsForOperation(AbstractTree<T> t, String operationName, long numberOfOperationCalls) {
+//        System.out.println("Average number of comparisons per " + operationName + ": " + (double)t.getNumberOfComparisons() / numberOfOperationCalls + " comparisons");
+        System.out.printf("%.3f\n",(double)t.getNumberOfComparisons() / numberOfOperationCalls);
+//        System.out.println("Average number of node modifications per " + operationName + ": " + (double)t.getNumberOfNodeModifications() / numberOfOperationCalls + " modifications");
+        System.out.printf("%.3f\n",(double)t.getNumberOfNodeModifications() / numberOfOperationCalls);
+//        System.out.println();
+    }
+
     public static long measureTime(Runnable func) {
         long start = System.nanoTime();
         func.run();
         long end = System.nanoTime();
-        return end-start;
+        return end - start;
     }
 }
